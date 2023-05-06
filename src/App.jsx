882ce0f05a82ee097890263edbfa7348dcc9b8f8
Kagiso31 <kagisomokou@gmail.com>
2023-05-06 14:13:27 +0200

    Add theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import ThemeToggle from "./ThemeToggle";
+
 const App = () => {
-  return ( 
-    <div>Hello, World!</div>
-   );
-}
- 
+  return (
+    <div>
+      <div className="header">
+        <h1>T O D O</h1>
+        <ThemeToggle />
+      </div>
+    </div>
+  );
+};
+
 export default App;
